be44c06715a90dda885fe428d97caed1c0f21109
Giannioq <giannioquendo@gmail.com>
2023-02-22 17:06:32 -0800

    hello

---
+++ soil_sensor_2.ino
@@ -0,0 +1,9 @@
+
+/* ## CODE TO MAKE SURE SOIL SENSOR IS WORKING ##
+##Values for moisture levels
+## 0 to 300 = dry soil 
+## 300 to 700 = humid soil 
+## 700 to 950 = wet soil*/
+
+#define LED_PIN 15
+#define
